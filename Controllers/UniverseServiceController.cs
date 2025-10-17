302cccfdf8c0834afdf8ee4b7397f41efd85b3d6
shmykov.alexander <shmykov.alexander@gmail.com>
2025-10-17 11:32:21 +0300

    Revert "Show Vera"
    This reverts commit cacb82f2c6ec37a4bd779e79a69185ef0908424f.

--- Controllers/UniverseServiceController.cs
+++ Controllers/UniverseServiceController.cs
@@ -35,7 +35,7 @@ namespace ClipBoardHelper.Controllers
                 functions: new List<Formula_One_Function>
                 {
                     new Formula_One_Function(
-                        names: new List<string> { "@select", "@выбратьX" },
+                        names: new List<string> { "@select", "@выбрать" },
                         isOperator: true,
                         descr: "аналог sql select",
                         visible: true,
